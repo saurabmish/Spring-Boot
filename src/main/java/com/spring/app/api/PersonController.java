db841bbec52d9b0132b44fab46bb1de1a8a0777d
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-10 10:51:01 -0800

    added annotations for POST request and API endpoint

--- src/main/java/com/spring/app/api/PersonController.java
+++ src/main/java/com/spring/app/api/PersonController.java
@@ -4,7 +4,13 @@ import com.spring.app.model.Person;
 import com.spring.app.service.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
 
     private final PersonService personService;
@@ -14,7 +20,8 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
