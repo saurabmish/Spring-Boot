e216ccf24c69917658affcfeef33f1ae504e5ffc
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-11 15:37:40 -0800

    POST temporary data using curl

---
+++ src/test/java/com/spring/app/post_people.sh
@@ -0,0 +1,40 @@
+curl --verbose \
+    --header 'Content-Type: application/json' 'Accept: application/json' \
+    --data '
+        {
+            "name": "Adam"
+        }' \
+    http://127.0.0.1:8080/api/v1/person
+
+
+curl --verbose \
+    --header 'Content-Type: application/json' 'Accept: application/json' \
+    --data '
+        {
+            "name": "Jennifer"
+        }' \
+    http://127.0.0.1:8080/api/v1/person
+
+
+curl --verbose \
+    --header 'Content-Type: application/json' 'Accept: application/json' \
+    --data '
+        {
+            "name": "Jason"
+        }' \
+    http://127.0.0.1:8080/api/v1/person
+
+
+curl --verbose \
+    --header 'Content-Type: application/json' 'Accept: application/json' \
+    --data '
+        {
+            "name": "Hannah"
+        }' \
+    http://127.0.0.1:8080/api/v1/person
+
+
+curl --include \
+    --header 'Content-Type: application/json' 'Accept: application/json' \
+    --request GET \
+    http://127.0.0.1:8080/api/v1/person
